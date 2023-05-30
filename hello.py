2cdb340eddcda020886cba52e0a3c9afe31e6544
Riload <ricardovlmmarcos@gmail.com>
2023-05-30 20:08:11 +0200

    primer proyecto

--- hello.py
+++ hello.py
@@ -1,19 +1,29 @@
 import pygame
-
-## Creacion de la ventan del juego
+pygame.init()
+## Creacion de la ventana del juego
 pantalla = pygame.display.set_mode((800,600))
+
+metronomo = pygame.time.Clock()
+
 
 # Titulo de pantalla
 pygame.display.set_caption('Hola Mundo')
 game_over =  False
+azul = 0
 # Montar el bucle principal
-while True:
+while game_over == False:
+    metronomo.tick(60)
+
     ## vaciar el buffer de eventos
-    for evento in pygame.get():
+    for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             game_over = True
         
+    azul += 1
+    if azul > 255:
+        azul = 0
+        
     # color de pantalla
-    pantalla.fill((0,0,255))
+    pantalla.fill((0,0,azul))
     # mostrar en pantalla
     pygame.display.flip()
